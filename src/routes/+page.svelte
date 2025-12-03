c48f73b721521ea86592e0d983fff24595e3eea1
Mark B <markkbarrett@gmail.com>
2025-12-03 13:07:38 -0800

    add simple homepage

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to Intro to Web Development</h1>
+<p>Here you can see the basics of web development in practice, based on my learning from a web development course at <a href="https://lacc.edu">Los Angeles City College</a></p>
+<p>This site is built with SvelteKit. Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
