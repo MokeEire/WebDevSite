8c6d5cee43f78fa885143d597f0500f100fbc35f
Mark B <markkbarrett@gmail.com>
2025-12-03 13:08:39 -0800

    create sidebar navigation component

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { modules } from '$lib/data/modules.js';
+	import { page } from '$app/stores'
+</script>
+
+<nav>
+	<ul>
+		<li><a href="/" class:active={$page.url.pathname === '/'}>Home</a></li>
+		{#each modules as module}
+			<li><a href="/modules/{module.slug}" class:active={$page.url.pathname === `/modules/${module.slug}`}>{module.number}. {module.title}</a></li>
+		{/each}
+	</ul>
+</nav>
+
+<style>
+	nav ul li a {
+		border-radius: 16px;
+		padding: 4px 8px;
+	}
+	.active {
+		font-weight: bold;
+		background-color: blue;
+		color: white;
+	}
+</style>
