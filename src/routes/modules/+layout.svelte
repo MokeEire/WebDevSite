6df31c03d1500b86f0afc6c44082da07fbd3fe21
Mark B <markkbarrett@gmail.com>
2025-12-05 13:23:30 -0800

    add number and description to module pages

--- src/routes/modules/+layout.svelte
+++ src/routes/modules/+layout.svelte
@@ -7,11 +7,21 @@
     const { currentModule } = $page.data;
 
     let pageTitle = $derived(
-        currentModule?.title || 'Module Not Found'
+        `${currentModule.number}. ${currentModule.title}` || 'Module Not Found'
+    )
+
+    let moduleDesc = $derived(
+        currentModule?.description || 'Module Not Found'
     )
 </script>
 
 <h1>{pageTitle}</h1>
+
+<p>
+    {moduleDesc}
+</p>
+
+<hr>
 
 <div class="module">
     {@render children()}
@@ -22,5 +32,11 @@
         font-weight: 700;
         font-size: 2rem;
         margin-bottom: 8px;
+    }
+
+    hr {
+        margin: 16px 0;
+        color: grey;
+        opacity: 0.6;
     }
 </style>
