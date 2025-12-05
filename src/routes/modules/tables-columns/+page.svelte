f73a79738b5990b0c6ff140249aa190e950477b4
Mark B <markkbarrett@gmail.com>
2025-12-05 12:29:27 -0800

    add start of tables and columns page

--- src/routes/modules/tables-columns/+page.svelte
+++ src/routes/modules/tables-columns/+page.svelte
@@ -1,1 +1,101 @@
-<h1>Working with Tables and Columns</h1>
+<script>
+	import MDNLink from '$lib/components/MDNLink.svelte';
+	import ExampleTable from './ExampleTable.svelte'
+</script>
+
+<p>
+	In this module, I'm learning how to use the <MDNLink element="table" /> HTML structure to display information
+	in a tabular format.
+	Any good table has rows, and that's our starting point in HTML. 
+	In the textbook, the table structure is built out by adding rows to the table with <MDNLink element="tr" />, 
+	then adding cells to the rows with <MDNLink	element="td"/>,
+	and later getting to row groups using <MDNLink element="thead" />,
+	<MDNLink element="tbody" />, and <MDNLink element="tfoot" />. 
+	As I learned while writing this, you can't just put rows directly into a table - they need to be in one of the row groups.
+	(check the <MDNLink	element="tr"/> element page to see why). 
+	So I can't just do this:
+</p>
+<pre>
+    <code class="language-html">
+        &lt;table&gt;
+            &lt;tr&gt; table cells &lt;/tr&gt;
+            &lt;tr&gt; table cells &lt;/tr&gt;
+            ...
+        &lt;/table&gt;
+    </code>
+</pre>
+
+<p>
+	Instead, I need to put the text into cells, and the cells into the rows, and then group the rows
+	in a <MDNLink element="tbody" /> element. 
+	Here's the more complete code for a simple table with two rows:
+</p>
+
+<pre>
+    <code class="language-html">
+        &lt;table&gt;
+            &lt;tbody&gt;
+                &lt;tr&gt;
+                    &lt;td&gt;table cells&lt;/td&gt;
+                &lt;/tr&gt;
+                &lt;tr&gt;
+                    &lt;td&gt;table cells&lt;/td&gt;
+                &lt;/tr&gt;
+            &lt;/tbody&gt;
+        &lt;/table&gt;
+    </code>
+</pre>
+
+<p>Which looks like this:</p>
+
+<div class="code-example">
+	<table>
+		<tbody>
+			<tr>
+				<td>table cells</td>
+			</tr>
+			<tr>
+				<td>table cells</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+
+<p>
+	But I will try to break down the example in the same way the textbook does.
+	The textbook uses a radio station schedule, so I will use my <a href="https://laist.com/program-schedule">local NPR
+	station's schedule as an example. (I'm only going to do a portion of the schedule for simplicity)
+	Below is the table we'll be building towards:
+</p>
+
+<ExampleTable />
+
+
+
+<h2>Borders</h2>
+
+<h2>Spanning Rows and Columns</h2>
+
+<h2>Adding a Caption</h2>
+
+<h2>Row groups</h2>
+
+<h2>Column groups</h2>
+
+
+
+<p>
+	Next we'll add borders and get into spanning rows and columns. But it will be helpful to have some
+	data to work with. 
+</p>
+
+<p>This table uses:</p>
+<ul>
+	<li>a <MDNLink element="thead" /> element for header groups</li>
+	<li><MDNLink element="th" /> elements for header cells</li>
+	<li><code>colspan</code> and <code>rowspan</code> attributes of the <MDNLink element="td" /> element to span cells across multiple columns</li>
+</ul>
+
+<style>
+	
+</style>
---
+++ src/routes/modules/tables-columns/ExampleTable.svelte
@@ -0,0 +1,117 @@
+<table class="laist-schedule">
+	<caption>Times are in Pacific Time (PT)</caption>
+	<colgroup>
+		<col id="time-column" />
+		<col class="day-column" span="5" />
+	</colgroup>
+	<thead>
+		<tr>
+			<th>Time</th>
+			<th>Monday</th>
+			<th>Tuesday</th>
+			<th>Wednesday</th>
+			<th>Thursday</th>
+			<th>Friday</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<th>5:00 PM</th>
+			<td colspan="5" rowspan="3">All Things Considered</td>
+		</tr>
+		<tr>
+			<th>5:30 PM</th>
+		</tr>
+		<tr>
+			<th>6:00 PM</th>
+		</tr>
+		<tr>
+			<th>6:30 PM</th>
+			<td colspan="5">Marketplace</td>
+		</tr>
+		<tr>
+			<th>7:00 PM</th>
+			<td rowspan="2">1A</td>
+			<td rowspan="6">Fresh Air</td>
+			<td rowspan="2" colspan="3">1A</td>
+		</tr>
+		<tr>
+			<th>7:30 PM</th>
+		</tr>
+		<tr>
+			<th>8:00 PM</th>
+			<td rowspan="2">Fresh Air</td>
+			<td rowspan="2" colspan="3">Fresh Air</td>
+		</tr>
+		<tr>
+			<th>8:30 PM</th>
+		</tr>
+		<tr>
+			<th>9:00 PM</th>
+			<td rowspan="2">On Point</td>
+			<td rowspan="2" colspan="2">On Point</td>
+			<td rowspan="2">Science Friday</td>
+		</tr>
+		<tr>
+			<th>9:30 PM</th>
+		</tr>
+		<tr>
+			<th>10:00 PM</th>
+			<td rowspan="2" colspan="3">BBC World Service</td>
+			<td rowspan="2">The Middle</td>
+			<td rowspan="2">Film Week</td>
+		</tr>
+		<tr>
+			<th>10:30 PM</th>
+		</tr>
+	</tbody>
+</table>
+
+<style>
+    /* Example table styles */
+	table.laist-schedule {
+		margin: 16px auto;
+		border-collapse: collapse;
+		border-radius: 4px;
+		border-right: hidden;
+		width: 100%;
+		caption-side: bottom;
+		background-size: cover;
+		background-repeat: no-repeat;
+		table-layout: fixed;
+		background-image: url('https://scpr.brightspotcdn.com/resource/00000178-22b1-dfd0-a9fc-b3ff1f850000/styleguide/assets/graphics/gradient-sunset.43fbb1935243de0b8b6fa7757d19efb2.svg');
+	}
+
+	table.laist-schedule caption {
+		margin-top: 8px;
+		font-style: italic;
+		text-align: center;
+	}
+
+	table.laist-schedule thead tr {
+		border-bottom: 2px solid #000;
+		height: 28px;
+	}
+
+	table.laist-schedule tbody tr {
+		border-bottom: 1px solid #000;
+		height: 32px;
+	}
+
+	table.laist-schedule tbody tr td {
+		border: 1px solid #3b3939;
+		text-align: center;
+	}
+
+	table.laist-schedule #time-column {
+		width: 10%;
+		text-align: center;
+		font-weight: bold;
+		border-right: 2px solid #000;
+		background-color: white;
+	}
+	table.laist-schedule col.day-column {
+		width: 18%;
+		border-right: 1px solid #000;
+	}
+</style>
