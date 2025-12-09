4172f3470d1d9e8e1ca7c57cb348aefdc829190e
Mark B <markkbarrett@gmail.com>
2025-12-09 15:56:02 -0800

    add separate pages for about and building the site

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,12 @@
+<h1>Welcome to Intro to Web Development</h1>
+<p>
+    Here you can see the basics of web development in practice, 
+    based on my learning from a web development course at <a href="https://lacc.edu">Los Angeles City College</a>. 
+    The textbook used for the course is <cite>New Perspectives on HTML5 and CSS3, Comprehensive 8th Edition</cite> by Patrick Carey.
+    You can find it 
+    <a href="https://www.cengage.com/c/new-perspectives-on-html-5-and-css-comprehensive-comprehensive-8e-carey/9780357107140/">on Cengage</a> 
+    or elsewhere on the internet.
+</p>
+<p>
+    You can explore the content in the course modules, or you can read about <a href="/building-the-site">how this site is built</a>.
+</p>
---
+++ src/routes/building-the-site/+page.svelte
@@ -0,0 +1,5 @@
+<h1>How this site is built</h1>
+
+<p>This site is built with SvelteKit. 
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation.
+</p>
