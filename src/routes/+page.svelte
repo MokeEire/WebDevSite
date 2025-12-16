1f3b86bda32d4bb774503b5690262bae89bf6cb5
Mark B <markkbarrett@gmail.com>
2025-12-15 22:15:05 -0800

    link to pages on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,10 @@
     Here you can see the basics of web development in practice, 
     based on my learning from a web development course at <a href="https://lacc.edu">Los Angeles City College</a>. 
     The textbook used for the course is <cite>New Perspectives on HTML5 and CSS3, Comprehensive 8th Edition</cite> by Patrick Carey.
-    You can find it <a href="https://www.cengage.com/c/new-perspectives-on-html-5-and-css-comprehensive-comprehensive-8e-carey/9780357107140/">on Cengage</a> or elsewhere on the internet.
+    You can find it 
+    <a href="https://www.cengage.com/c/new-perspectives-on-html-5-and-css-comprehensive-comprehensive-8e-carey/9780357107140/">on Cengage</a> 
+    or elsewhere on the internet.
 </p>
-<p>This site is built with SvelteKit. Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+    You can explore the content in the course modules, or you can read about <a href="/building-the-site">how this site is built</a>.
+</p>
